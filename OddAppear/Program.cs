31a969d942f8abf6a7072ce30b2ac746b5b5716e
Burapa.Tong <56977274+TongBurapa@users.noreply.github.com>
2024-04-29 03:35:02 +0700

    Delete OddAppear directory

--- OddAppear/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using OddAppear;
-
-var oddAppearClass = new OddAppearClass();
-int[] input = { 2, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 1 };
-var result = oddAppearClass.FindOddAppear(input);
-Console.WriteLine(result);
-
-
